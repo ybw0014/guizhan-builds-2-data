2da72cec47bb3da2cb6a9f0dd4a8e044490669f2
ybw0014 <i@guizhanss.com>
2023-07-13 18:12:14 -0400

    fix: avoid invalid entries

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-import { ProjectBuildOptions } from './ProjectBuildOptions'
-import { ProjectDisplayOptions } from './ProjectDisplayOptions'
-import { RawProject } from './RawProject'
-import { Project } from './Project'
-import { Projects } from './Projects'
-import { BuildInfo } from './BuildInfo'
-import { BuildsInfo } from './BuildsInfo'
-import { LegacyBuildInfo } from './LegacyBuildInfo'
-import { LegacyBuildsInfo } from './LegacyBuildsInfo'
+import { ProjectBuildOptions } from "./ProjectBuildOptions";
+import { ProjectDisplayOptions } from "./ProjectDisplayOptions";
+import { RawProject } from "./RawProject";
+import { Project } from "./Project";
+import { Projects } from "./Projects";
+import { BuildInfo } from "./BuildInfo";
+import { BuildsInfo } from "./BuildsInfo";
+import { LegacyBuildInfo } from "./LegacyBuildInfo";
+import { LegacyBuildsInfo } from "./LegacyBuildsInfo";
 
-export { ProjectBuildOptions, ProjectDisplayOptions, RawProject, Project, Projects, BuildInfo, BuildsInfo, LegacyBuildInfo, LegacyBuildsInfo }
+export { ProjectBuildOptions, ProjectDisplayOptions, RawProject, Project, Projects, BuildInfo, BuildsInfo, LegacyBuildInfo, LegacyBuildsInfo };
 
 export function useParseProjects(rawProjects: Projects): Project[] {
-  const projects: Project[] = []
+  const projects: Project[] = [];
   for (const [key, rawProject] of Object.entries(rawProjects)) {
-    const [author, repoNBranch] = key.split('/')
-    const [repository, branch] = repoNBranch.split(':')
+    if (!key.includes("/") && !key.includes(":")) continue;
+    const [author, repoNBranch] = key.split("/");
+    const [repository, branch] = repoNBranch.split(":");
     const project: Project = {
       key,
       author,
       repository,
       branch,
       ...(rawProject as RawProject),
-    }
-    projects.push(project)
+    };
+    projects.push(project);
   }
-  return projects
+  return projects;
 }
